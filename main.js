4cc235c68f8e0a5ed556172a55f2e43c0acee33b
Enis Gjini <egjini17@gmail.com>
2024-02-26 00:07:19 +0100

    #

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow, ipcMain, Tray, clipboard } = require("electron");
 const activeWin = require("active-win");
 const fs = require("fs");
-const path = require("path");
-const process = require("process");
-const { authenticate } = require("@google-cloud/local-auth");
-const { google } = require("googleapis");
 
 let mainWindow;
-let csvFilePath = path.join(process.cwd(), "activity.csv"); // Set absolute path for CSV file
+let csvFilePath = "activity.csv";
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -17,6 +13,7 @@ function createWindow() {
       nodeIntegration: true,
       contextIsolation: false, // Required to use ipcRenderer
     },
+    icon: "logo.png",
   });
 
   // Maximize the window to full-screen
@@ -29,100 +26,6 @@ function createWindow() {
   });
 }
 
-// If modifying these scopes, delete token.json.
-const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = path.join(process.cwd(), "token.json");
-const CREDENTIALS_PATH = path.join(process.cwd(), "credentials.json");
-
-/**
- * Reads previously authorized credentials from the save file.
- *
- * @return {Promise<OAuth2Client|null>}
- */
-async function loadSavedCredentialsIfExist() {
-  try {
-    const content = await fs.promises.readFile(TOKEN_PATH);
-    const credentials = JSON.parse(content);
-    return google.auth.fromJSON(credentials);
-  } catch (err) {
-    return null;
-  }
-}
-
-/**
- * Serializes credentials to a file compatible with GoogleAUth.fromJSON.
- *
- * @param {OAuth2Client} client
- * @return {Promise<void>}
- */
-async function saveCredentials(client) {
-  const content = await fs.promises.readFile(CREDENTIALS_PATH);
-  const keys = JSON.parse(content);
-  const key = keys.installed || keys.web;
-  const payload = JSON.stringify({
-    type: "authorized_user",
-    client_id: key.client_id,
-    client_secret: key.client_secret,
-    refresh_token: client.credentials.refresh_token,
-  });
-  await fs.promises.writeFile(TOKEN_PATH, payload);
-}
-
-/**
- * Load or request or authorization to call APIs.
- *
- */
-async function authorize() {
-  let client;
-  try {
-    client = await loadSavedCredentialsIfExist();
-    if (client) {
-      return client;
-    }
-    client = await authenticate({
-      scopes: SCOPES,
-      keyfilePath: CREDENTIALS_PATH,
-    });
-    if (client.credentials) {
-      await saveCredentials(client);
-    }
-    return client;
-  } catch (error) {
-    console.error("Authorization failed:", error);
-    throw error;
-  }
-}
-
-/**
- * Lists the next 10 events on the user's primary calendar.
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-async function listEvents(auth) {
-  const calendar = google.calendar({ version: "v3", auth });
-  const res = await calendar.events.list({
-    calendarId: "primary",
-    timeMin: new Date().toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: "startTime",
-  });
-  const events = res.data.items;
-  if (!events || events.length === 0) {
-    console.log("No upcoming events found.");
-    return;
-  }
-  console.log("Upcoming 10 events:");
-  events.map((event, i) => {
-    const start = event.start.dateTime || event.start.date;
-    console.log(`${start} - ${event.summary}`);
-  });
-}
-
-authorize().then(listEvents).catch(console.error);
-
 function saveToCSV(data) {
   fs.appendFile(csvFilePath, data + "\n", (err) => {
     if (err) {
@@ -133,13 +36,13 @@ function saveToCSV(data) {
 
 async function sendActiveWindowInfo() {
   try {
-    const activeWindowInfo = await activeWin();
+    const activeWindow = await activeWin();
     const currentTime = new Date().toLocaleString();
     // Extract only the application name from the owner property
-    const applicationName = activeWindowInfo.owner.name.split(" - ")[0];
+    const applicationName = activeWindow.owner.name.split(" - ")[0];
     const data = `${currentTime},${applicationName}`;
     saveToCSV(data);
-    mainWindow.webContents.send("activeWindow", data); // Send active window data to mainWindow
+    mainWindow.webContents.send("activeWindow", data);
   } catch (error) {
     console.error("Error retrieving active window:", error);
   }
@@ -152,12 +55,21 @@ function loadTimelineData() {
       return;
     }
     console.log("Data from CSV:", data);
-    mainWindow.webContents.send("activeWindow", data); // Send CSV data to mainWindow
+    mainWindow.webContents.send("activeWindow", data);
+
+    // Parse the CSV data and send it line by line
+    const lines = data.split("\n");
+    lines.forEach((line) => {
+      mainWindow.webContents.send("activeWindow", line);
+    });
   });
 }
 
 app.on("ready", () => {
   createWindow();
+
+  // Create tray icon
+  const appIcon = new Tray("logo.png");
 
   // Send active window information every second
   setInterval(sendActiveWindowInfo, 1000);
