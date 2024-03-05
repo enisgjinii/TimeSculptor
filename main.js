037a707ea8cab507df09dd332e9c007ead916f02
Enis Gjini <egjini17@gmail.com>
2024-03-05 15:33:56 +0100

    #

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 const { app, BrowserWindow, ipcMain, Tray, clipboard } = require("electron");
 const activeWin = require("active-win");
 const fs = require("fs");
-
 let mainWindow;
 let csvFilePath = "activity.csv";
-
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
@@ -15,17 +13,13 @@ function createWindow() {
     },
     icon: "logo.png",
   });
-
   // Maximize the window to full-screen
   mainWindow.maximize();
-
   mainWindow.loadFile("index.html");
-
   mainWindow.on("closed", function () {
     mainWindow = null;
   });
 }
-
 function saveToCSV(data) {
   fs.appendFile(csvFilePath, data + "\n", (err) => {
     if (err) {
@@ -33,7 +27,6 @@ function saveToCSV(data) {
     }
   });
 }
-
 async function sendActiveWindowInfo() {
   try {
     const activeWindow = await activeWin();
@@ -47,7 +40,6 @@ async function sendActiveWindowInfo() {
     console.error("Error retrieving active window:", error);
   }
 }
-
 function loadTimelineData() {
   fs.readFile(csvFilePath, "utf8", (err, data) => {
     if (err) {
@@ -56,7 +48,6 @@ function loadTimelineData() {
     }
     console.log("Data from CSV:", data);
     mainWindow.webContents.send("activeWindow", data);
-
     // Parse the CSV data and send it line by line
     const lines = data.split("\n");
     lines.forEach((line) => {
@@ -64,28 +55,22 @@ function loadTimelineData() {
     });
   });
 }
-
 app.on("ready", () => {
   createWindow();
-
   // Create tray icon
   const appIcon = new Tray("logo.png");
-
   // Send active window information every second
   setInterval(sendActiveWindowInfo, 1000);
-
   // Handle request to load timeline data
   ipcMain.on("loadTimeline", () => {
     loadTimelineData();
   });
 });
-
 app.on("window-all-closed", function () {
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
-
 app.on("activate", function () {
   if (mainWindow === null) {
     createWindow();
