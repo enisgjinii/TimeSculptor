ee0ecea8c4b8c659092116feea766b34414bbfa3
Enis Gjini <egjini17@gmail.com>
2024-03-12 11:16:04 +0100

    This commit updates the fetch URL and template string in the HTML file, as well as the sidebar styling and functionality.

--- api/get_date_from_flatpickr.js
+++ api/get_date_from_flatpickr.js
@@ -1,9 +1,12 @@
 // Initialize currentDateForCategories
 let currentDateForCategories = new Date();
+
 // Function to fetch data from the endpoint
 function fetchCategoryData(startDate, endDate) {
   // Fetch data from the endpoint
-  fetch(`http://localhost:3000/category_usage_by_date/${startDate}/${endDate}`)
+  fetch(
+    `http://localhost:${PORT}/category_usage_by_date/${startDate}/${endDate}`
+  )
     .then((response) => response.json())
     .then((data) => {
       // Access the <ul> element
@@ -19,7 +22,7 @@ function fetchCategoryData(startDate, endDate) {
         liElement.className =
           "flex justify-between border-b border-gray-400 py-2"; // Flex container with space between items and border
         // Set the text content of the <li> element
-        liElement.innerHTML = `<span class="text-right text-gray-500">${category}</span><span class="text-gray-500">${hours} hours, ${minutes} minutes</span>`;
+        liElement.innerHTML = `<span class="text-right text-gray-500 dark:text-gray-400">${category}</span><span class="text-gray-500 dark:text-gray-400">${hours} hours, ${minutes} minutes</span>`;
         // Append the <li> element to the <ul> element
         ulElement.appendChild(liElement);
       }
@@ -28,23 +31,7 @@ function fetchCategoryData(startDate, endDate) {
       console.error("Error fetching category usage data:", error);
     });
 }
-// Initialize flatpickr date picker for start and end dates
-flatpickr("#startDate", {
-  dateFormat: "Y-m-d",
-  onChange: function (selectedDates, dateStr) {
-    const startDate = dateStr;
-    const endDate = document.getElementById("endDate").value;
-    fetchCategoryData(startDate, endDate);
-  },
-});
-flatpickr("#endDate", {
-  dateFormat: "Y-m-d",
-  onChange: function (selectedDates, dateStr) {
-    const endDate = dateStr;
-    const startDate = document.getElementById("startDate").value;
-    fetchCategoryData(startDate, endDate);
-  },
-});
+
 // Initialize fetch data with today's date
 const today = new Date();
 const year = today.getFullYear();
@@ -52,7 +39,11 @@ const month = String(today.getMonth() + 1).padStart(2, "0");
 const day = String(today.getDate()).padStart(2, "0");
 const startDate = `${year}-${month}-${day}`;
 const endDate = `${year}-${month}-${day}`;
+
+const PORT = "3000"; // Replace {{PORT}} with the actual port number
+
 fetchCategoryData(startDate, endDate);
+
 // Event listeners
 document.getElementById("back-button").addEventListener("click", () => {
   currentDateForCategories.setDate(currentDateForCategories.getDate() - 1);
@@ -61,10 +52,8 @@ document.getElementById("back-button").addEventListener("click", () => {
     new Date(currentDateForCategories.getTime())
   ); // Create a copy of the date object
   fetchCategoryData(startDate, endDate);
-  // Clear the start date and end date inputs
-  document.getElementById("startDate")._flatpickr.clear();
-  document.getElementById("endDate")._flatpickr.clear();
 });
+
 document.getElementById("today-button").addEventListener("click", () => {
   currentDateForCategories = new Date();
   const startDate = formatDateForCategory(currentDateForCategories);
@@ -72,10 +61,8 @@ document.getElementById("today-button").addEventListener("click", () => {
     new Date(currentDateForCategories.getTime())
   ); // Create a copy of the date object
   fetchCategoryData(startDate, endDate);
-  // Clear the start date and end date inputs
-  document.getElementById("startDate")._flatpickr.clear();
-  document.getElementById("endDate")._flatpickr.clear();
 });
+
 document.getElementById("next-button").addEventListener("click", () => {
   currentDateForCategories.setDate(currentDateForCategories.getDate() + 1);
   const startDate = formatDateForCategory(currentDateForCategories);
@@ -83,10 +70,8 @@ document.getElementById("next-button").addEventListener("click", () => {
     new Date(currentDateForCategories.getTime())
   ); // Create a copy of the date object
   fetchCategoryData(startDate, endDate);
-  // Clear the start date and end date inputs
-  document.getElementById("startDate")._flatpickr.clear();
-  document.getElementById("endDate")._flatpickr.clear();
 });
+
 // Function to format date in "YYYY-MM-DD" format
 function formatDateForCategory(date) {
   const year = date.getFullYear();
